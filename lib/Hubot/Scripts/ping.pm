a234e7deca82119c04243fe47f2680401688b151
Hyungsuk Hong <aanoaa@gmail.com>
2012-10-05 14:44:17 +0900

    feature - `hubot: die`

--- lib/Hubot/Scripts/ping.pm
+++ lib/Hubot/Scripts/ping.pm
@@ -5,6 +5,14 @@ use warnings;
 sub load {
     my ( $class, $robot ) = @_;
     $robot->respond( qr/ping$/i, sub { shift->reply('PONG') } );
+    $robot->respond(
+        qr/die$/i,
+        sub {
+            shift->send('Goodbye, cruel world.');
+            $robot->shutdown;
+            exit;
+        }
+    );
 }
 
 1;
