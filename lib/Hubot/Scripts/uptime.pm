8fec7659bc365ea8facf6a50888467002b8765d7
Yunchang Kang <codenewb@gmail.com>
2013-06-27 17:06:14 +0900

    Add StartTime, StopTime and SleepTime func at uptime.pm

--- lib/Hubot/Scripts/uptime.pm
+++ lib/Hubot/Scripts/uptime.pm
@@ -7,7 +7,7 @@ use DateTime::Format::Duration;
 
 sub load {
     my ( $class, $robot ) = @_;
-    my $start = DateTime->now;
+    my $start = DateTime->now( time_zone => 'local');
     $robot->respond(
         qr/uptime/i,
         sub {
@@ -15,11 +15,12 @@ sub load {
             my $ymd = $start->ymd('/');
             my $hms = $start->hms;
             my @ymdhms = "$ymd-$hms";
+
             if ( -f 'ymdhms.dat' ) {
                 my $started_time = retrieve('ymdhms.dat');
-                my $die_during_time = $started_time->[0];
-                $msg->send('Hubot die time ' . $started_time->[0]);
-                $msg->send('Hubot start time ' . "@ymdhms");
+                sleepTime($msg, $started_time->[0], $ymdhms[0]);
+                $msg->send('Stop Time: ' . $started_time->[0]);
+                $msg->send('Start Time ' . "@ymdhms");
                 uptimeMe( $msg, $start, sub { $msg->send(shift) } );
             }
             store \@ymdhms, 'ymdhms.dat';
@@ -39,8 +40,17 @@ sub uptimeMe {
     $cb->( "I've been sentient for " . $d->format_duration($duration) );
 }
 
-sub stop_during_time {
-        
+sub sleepTime {
+    my ( $msg, $die_time, $start_time ) = @_;
+    my @d_time = $die_time =~ m{(\d+)/(\d+)/(\d+)-(\d+):(\d+):(\d+)};
+    my @s_time = $start_time =~ m{(\d+)/(\d+)/(\d+)-(\d+):(\d+):(\d+)};
+    my $cnt = 0;
+    my @n_time;
+    for my $t_par ( @d_time) {
+        $n_time[$cnt] = abs($s_time[$cnt] - $t_par);
+        $cnt++;
+    }
+    $msg->send('Sleep Time: '. "@n_time");
 }
 
 1;
