328fc085a1f8494d6ca054ab6f5759c764b6f34e
Hyungsuk Hong <aanoaa@gmail.com>
2014-06-24 23:33:03 +0900

    update dzil profile

--- misc/dzil/profiles/hubot/Module.pm
+++ misc/dzil/profiles/hubot/Module.pm
@@ -1,12 +1,3 @@
-package { {$name} };
-{
-    {
-        use strict;
-        use vars '$foo';
-        $Text::Template::SILENTLY;    # same as '';
-    }
-}
-
 # ABSTRACT: {{$name}}
 use strict;
 use warnings;
@@ -59,13 +50,12 @@ need more description?
 
 =head1 AUTHOR
 
-Your name <your@email.com>
+Hyungsuk Hong <hshong@perl.kr>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by <Your Name>.
+The MIT License (MIT)
 
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
+Copyright (c) 2014, Hyungsuk Hong
 
 =cut
